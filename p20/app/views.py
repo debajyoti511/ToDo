cd4f032296a669dc037474e2b190106e649f81aa
Debajyoti Naik <debajyotin56@gmail.com>
2025-01-31 20:29:01 +0530

    Add files via upload

---
+++ p20/app/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from .models import *
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+
+# Create your views here.
+def home(request):
+    todo_objects = Todo.objects.all()
+    d = {'todo_objects':todo_objects}
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        desc = request.POST.get('desc')
+        TO = Todo(title=title, desc=desc)
+        TO.save()
+    return render(request, 'home.html', d)
+
+
+def update(request, pk):
+    TO = Todo.objects.get(pk=pk)
+    d = {'TO':TO}
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        desc = request.POST.get('desc')
+        TO.title = title
+        TO.desc = desc
+        TO.save()
+        return HttpResponseRedirect(reverse('home'))
+    return render(request,'update.html', d)
+
+
+def delete(request, pk):
+    TO = Todo.objects.get(pk=pk)
+    TO.delete()
+    return HttpResponseRedirect(reverse('home'))
+
+
+
